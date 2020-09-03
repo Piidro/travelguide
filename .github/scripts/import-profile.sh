3ed337a53ee63ea64a83ee9fa1a40749634dc997
Petri Tilli <petri.e.tilli@gmail.com>
2020-09-03 18:01:07 +0300

    Added dev provisioning profile as the last resort

--- .github/scripts/import-profile.sh
+++ .github/scripts/import-profile.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo "$PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/profile.mobileprovision
+echo "$DEV_PROVISIONING_PROFILE_DATA" | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/dev_profile.mobileprovision
