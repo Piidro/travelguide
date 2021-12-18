9b3abb5f135d6f4dd057f7a44851f43b362f0c28
Petri Tilli <petri.e.tilli@gmail.com>
2021-12-18 18:12:55 +0200

    Initial Commit

---
+++ Travel Guide/Travel_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_GuideApp.swift
+//  Travel Guide
+//
+//  Created by Petri Tilli on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Travel_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
